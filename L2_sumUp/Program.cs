52fa46352897664f7997f9c844ebad4540ed5f2e
jana-mikesova <mikesovaj@gmail.com>
2024-04-24 22:36:57 +0200

    L2: sum up 2 numbers

---
+++ L2_sumUp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace L2_sumUp
+{
+    //Napište program, který se zeptá na dvě čísla a zobrazí jejich součet.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte první číslo:");
+            double firstNumber = getNumber(Console.ReadLine());
+
+            Console.WriteLine("Zadejte druhé číslo:");
+            double secondNumber = getNumber(Console.ReadLine());
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        }
+
+        public static double getNumber(string String)
+        {
+            bool isNumber = false;
+            double result = 0;
+            
+            while (!isNumber)
+            {
+                isNumber = double.TryParse(String, out result);
+                if (!isNumber)
+                {
+                    Console.WriteLine("Nebylo zadáno číslo, zadej znovu:");
+                    String = Console.ReadLine();
+                }
+            }
+            return result;
+        }
+    }
+}
