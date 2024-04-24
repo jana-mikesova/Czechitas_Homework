041dea7fa65c334b957bf6fb8a4835d692bcaecb
jana-mikesova <mikesovaj@gmail.com>
2024-04-24 23:49:28 +0200

    L2 archer shooting arrows

---
+++ L2_archer/Archer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2_archer
+{
+    internal class Archer
+    {
+        public int AmountOfArrows;
+
+        public Archer() 
+        {
+            AmountOfArrows = 10;
+        }
+
+        public Archer(int amountOfArrows)
+        {
+            AmountOfArrows = amountOfArrows;
+        }
+
+        public void shotArrow()
+        {            
+            if (AmountOfArrows > 0)
+            {
+                Console.WriteLine("Vždy se trefím přímo doprostřed!");               
+            } else
+            {
+                Console.WriteLine("Nemám šípy.");
+            }
+            AmountOfArrows--;
+        }
+    }
+}
---
+++ L2_archer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace L2_archer
+{
+    //Vytvořte třídu Lucistnik, ktera bude mit vlastnost PocetSipu a metodu Vystrel. PocetSipu muze byt nastaven treba na 10.
+    //Pokud bude mit dost sipu, napise metoda Vystrel na konzoli text: Vzdy se strefim primo do prostred!
+    //Metoda Vystrel uvnitr s kazdym vystrelem snizi pocet sipu.
+    //Pokud bude pocet 0, metoda Vystrel vypise "Nemam sipy".
+    //Napiste program, ktery vytvori lucistnika a vystreli vsechny sipy.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Archer archer = new Archer(5);
+
+            do
+            {
+                archer.shotArrow();
+            }
+            while (archer.AmountOfArrows >= 0);
+        }
+    }
+}
